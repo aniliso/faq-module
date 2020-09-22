65d260561a8b3146e53e38301c4701258ab6cc0b
sagsoz06 <designer@visualturk.net>
2020-09-22 03:08:47 +0300

    Update FAQ widgets (added category)

--- Entities/CategoryTranslation.php
+++ Entities/CategoryTranslation.php
@@ -10,8 +10,6 @@ class CategoryTranslation extends Model
     protected $fillable = ['name', 'slug', 'meta_title', 'meta_description'];
     protected $table = 'faq__category_translations';
 
-    protected $appends = ['url'];
-
     public function getUrlAttribute()
     {
         return localize_trans_url($this->locale, 'faq::routes.faq.category', ['slug'=>$this->slug]);
--- Entities/FaqTranslation.php
+++ Entities/FaqTranslation.php
@@ -10,8 +10,6 @@ class FaqTranslation extends Model
     protected $fillable = ['title', 'slug', 'content', 'meta_title', 'meta_description'];
     protected $table = 'faq__faq_translations';
 
-    protected $appends = ['url'];
-
     public function getUrlAttribute()
     {
         return localize_trans_url($this->locale, 'faq::routes.faq.slug', ['slug'=>$this->slug]);
